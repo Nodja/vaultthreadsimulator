a51845af869ad5a3ecfe9c2735090e27c04a7591
Nodja <me@nodja.com>
2015-08-24 00:10:59 -0400

    remove debug flag -_-

--- vaulthreadsimulator.py
+++ vaulthreadsimulator.py
@@ -26,4 +26,4 @@ def user(name):
     return render_template('index.html', posts=posts)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8010, debug=True)
+    app.run(host="0.0.0.0", port=8010)
